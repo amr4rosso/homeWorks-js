0b19a1eadf59019f11cf8338ae87fc6b6e09dcf4
amr4rosso <amr1979.aa@gmail.com>
2022-11-08 20:21:41 +0200

    Output the result to the console

--- hw2/js/scripts.js
+++ hw2/js/scripts.js
@@ -5,8 +5,25 @@ let userAge;
 let userName;
 let userSurname;
 
-let newUser;
-let shoppingList;
-let userOnline;
-let employeeSalary;
+let newUser = "Jack Reacher";
+console.log("New user: " + newUser);
+
+let shoppingList = "Bread, Coffee, tea and Sugar";
+console.log("my shopping list: " + shoppingList);
+
+let userOnline = true;
+
+if (true){
+    userOnline = "Online";
+}
+console.log("User: " + userOnline);
+
+
+let employeeSalary = 800;
+let currency = '$'
+console.log("salary employee is " + employeeSalary + currency);
+
 let cursorCoordinates;
+let xAxis = 45;
+let yAxis = 96;
+console.log("cursor coordinates: " + "X" + xAxis + " Y" + yAxis);
