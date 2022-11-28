49de5399984f8ce6dc8bac14c9ad16dc7a6060c8
Amrou Abid <amr1979.aa@gmail.com>
2022-11-28 11:39:10 +0200

    Update homework 6

--- hw-6/js/scripts.js
+++ hw-6/js/scripts.js
@@ -14,45 +14,117 @@ const makeFirstLetterCapital = (str) => {
 		
 };
 
+const userRegistrationName = () => {
 
-const userRegistration = () => {
 	let userName;
-	let userSurname; 
-	let userPassword;
-	let isAuthSuccess = false;
 
+	do {
+		userName = prompt('Введите вашу имя:');
+
+		if (!userName) {
+			alert('Введите хотя бы один срочний синвол! ');
+		} else {
+			break;
+		}
+
+	} while(true);
+
+	const upperCaseName = makeFirstLetterCapital(userName);
+
+	return upperCaseName;
+};
+
+
+const userRegistrationSurname = () => {
+
+	let userSurname;
+
+	do {
+		userSurname = prompt('Введите вашу фамилию:');
+
+		if (!userSurname) {
+			alert('Введите хотя бы один срочний синвол! ');
+		} else {
+			break;
+		}
+
+	} while(true);
+
+	const upperCaseSurname = makeFirstLetterCapital(userSurname);
+
+	return upperCaseSurname;
+};
+
+
+const userRegistrationPassword = () => {
+
+	let userPassword;
 
 	do {
 
-		userName = prompt("Enter your Name:");
-		if (!userName || userName.length < 1) {
-			alert("Your name must be more than 1 symbol");
-		  	continue;
-		}
+		userPassword = prompt('Придумайте пароль (не менее чем 6 символов и садержащий синволы разного регистра):');
 
-		userSurname = prompt("Enter your Surname:");
-		if (!userSurname || userSurname.length < 1) {
-			alert("Your surname must be more than 1 symbol");
-		  	continue;
-		}
-
-		userPassword = prompt("Enter your Password:");
-		if (!userPassword || userPassword.length < 6 ) {
-			alert("Your password must be more than 6 symbol");
-		  	continue;
-		}
-
-		if (userName && userSurname && userPassword) {
-			isAuthSuccess = true;
+		if (!userPassword || userPassword < 6) {
+			alert('Введите не менее 6 синволов!');
+		} else if (!isNaN(userPassword) || userPassword.toLocaleLowerCase() === userPassword || (userPassword.toLocaleUpperCase() === userPassword)) {
+			alert('НЕ корректный пароль!');
+		} else {
+			alert('Регистрация прошда успешно.');
 			break;
 		}
 
 
-	} while (!isAuthSuccess);
+	} while(true)
 
-	alert(`Welcome ${makeFirstLetterCapital(userName)} ${makeFirstLetterCapital(userSurname)}`);
+	return userPassword;
 
 };
+
+const restName = userRegistrationName();
+const restSurname = userRegistrationSurname();
+const restPassword = userRegistrationPassword()
+
+alert(`вы зарегистрированны под имени: ${restName} ${restSurname} Ваш пароль: ${restPassword}`);
+
+
+// const userRegistration = () => {
+// 	let userName;
+// 	let userSurname; 
+// 	let userPassword;
+// 	let isAuthSuccess = false;
+
+
+// 	do {
+
+// 		userName = prompt("Enter your Name:");
+// 		if (!userName || userName.length < 1) {
+// 			alert("Your name must be more than 1 symbol");
+// 		  	continue;
+// 		}
+
+// 		userSurname = prompt("Enter your Surname:");
+// 		if (!userSurname || userSurname.length < 1) {
+// 			alert("Your surname must be more than 1 symbol");
+// 		  	continue;
+// 		}
+
+// 		userPassword = prompt("Enter your Password:");
+// 		if (!userPassword || userPassword.length < 6 ) {
+// 			alert("Your password must be more than 6 symbol");
+// 		  	continue;
+// 		}
+
+// 		if (userName && userSurname && userPassword) {
+// 			isAuthSuccess = true;
+// 			break;
+// 		}
+
+
+// 	} while (!isAuthSuccess);
+
+// 	alert(`Welcome ${makeFirstLetterCapital(userName)} ${makeFirstLetterCapital(userSurname)}`);
+
+// };
 
 // userRegistration();
 
